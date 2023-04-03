0d7a70f9753071a8d7cec688f89b19867c0bea72
JonMS95 <jon_portugalete@hotmail.com>
2023-04-03 19:19:38 +0200

    Modified tests. Everything is fine.

--- Tests/Source_files/main.c
+++ Tests/Source_files/main.c
@@ -88,16 +88,15 @@ void ParseDemo(int argc, char** argv)
 
 typedef struct
 {
-    char opt_char[3]        ;
-    char* opt_long[3]       ;
-    char* opt_det[3]        ;
-    int opt_var_type[3]     ;
-    int opt_needs_arg[3]    ;
-    int opt_min_value[3]    ;
-    int opt_max_value[3]    ;
-    int opt_default_value[3];
-    int dest_var            ;
-    int* opt_dest_var[3]    ;
+    char            opt_char[2]        ;
+    char*           opt_long[3]       ;
+    char*           opt_det[3]        ;
+    int             opt_var_type[3]     ;
+    int             opt_needs_arg[4]    ;
+    OPT_DATA_TYPE   opt_min_value[2]    ;
+    OPT_DATA_TYPE   opt_max_value[3]    ;
+    OPT_DATA_TYPE   opt_default_value[2];
+    int*            opt_dest_var[2]    ;
 
 }GetOptionDefinition_Test_Data;
 
@@ -116,11 +115,11 @@ GetOptionDefinition_Test_Data GetOptionDefinition_data =
                                 NULL,
                                 "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
                             },
-    .opt_var_type =         {0, -1, 4},
-    .opt_needs_arg =        {1, -1, 3},
-    .opt_min_value =        {0, 2    },
-    .opt_max_value =        {2, 0    },
-    .opt_default_value =    {1, 3    },
+    .opt_var_type =         {0, -1, 4   },
+    .opt_needs_arg =        {1, -1, 3, 0},
+    .opt_min_value =        {0, 2       },
+    .opt_max_value =        {2, 0       },
+    .opt_default_value =    {1, 3       },
     .opt_dest_var =         {&dummy, NULL},
 
 };
@@ -128,19 +127,22 @@ GetOptionDefinition_Test_Data GetOptionDefinition_data =
 typedef int GetOptionDefinition_Test_Case[11];
 GetOptionDefinition_Test_Case GetOptionDefinition_Test_Indexes[]=
 {
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, GET_OPT_SUCCESS                     },
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, GET_OPT_ERR_NO_OPT_CHAR             },
-    {0, 1, 0, 0, 0, 0, 0, 0, 0, GET_OPT_ERR_NO_OPT_LONG             },
-    {0, 2, 0, 0, 0, 0, 0, 0, 0, GET_OPT_ERR_LONG_LENGTH_EXCEEDED    },
-    {0, 0, 1, 0, 0, 0, 0, 0, 0, GET_OPT_SUCCESS                     },
-    {0, 0, 2, 0, 0, 0, 0, 0, 0, GET_OPT_ERR_DETAIL_LENGTH_EXCEEDED  },
-    {0, 0, 0, 1, 0, 0, 0, 0, 0, GET_OPT_ERR_UNKNOWN_TYPE            },
-    {0, 0, 0, 2, 0, 0, 0, 0, 0, GET_OPT_ERR_UNKNOWN_TYPE            },
-    {0, 0, 0, 0, 1, 0, 0, 0, 0, GET_OPT_ERR_UNKNOWN_ARG_REQ         },
-    {0, 0, 0, 0, 2, 0, 0, 0, 0, GET_OPT_ERR_UNKNOWN_ARG_REQ         },
-    {0, 0, 0, 0, 0, 1, 1, 0, 0, GET_OPT_ERR_WRONG_BOUNDARIES        },
-    {0, 0, 0, 0, 0, 0, 0, 1, 0, GET_OPT_ERR_DEF_VAL_OUT_OF_BOUNDS   },
-    {0, 0, 0, 0, 0, 0, 0, 0, 1, GET_OPT_ERR_NULL_DEST_VAR           },
+//  opt_char    opt_long    opt_det opt_var_type    opt_needs_arg   opt_min_value   opt_max_value   opt_default_value   opt_dest_var    Error code
+    {0,         0,          0,      0,              0,              0,              0,              0,                  0,              GET_OPT_SUCCESS                     },
+    {0,         0,          0,      0,              0,              0,              0,              0,                  0,              GET_OPT_SUCCESS                     },
+    {1,         0,          0,      0,              0,              0,              0,              0,                  0,              GET_OPT_ERR_NO_OPT_CHAR             },
+    {0,         1,          0,      0,              0,              0,              0,              0,                  0,              GET_OPT_ERR_NO_OPT_LONG             },
+    {0,         2,          0,      0,              0,              0,              0,              0,                  0,              GET_OPT_ERR_LONG_LENGTH_EXCEEDED    },
+    {0,         0,          1,      0,              0,              0,              0,              0,                  0,              GET_OPT_SUCCESS                     },
+    {0,         0,          2,      0,              0,              0,              0,              0,                  0,              GET_OPT_ERR_DETAIL_LENGTH_EXCEEDED  },
+    {0,         0,          0,      1,              0,              0,              0,              0,                  0,              GET_OPT_ERR_UNKNOWN_TYPE            },
+    {0,         0,          0,      2,              0,              0,              0,              0,                  0,              GET_OPT_ERR_UNKNOWN_TYPE            },
+    {0,         0,          0,      0,              1,              0,              0,              0,                  0,              GET_OPT_ERR_UNKNOWN_ARG_REQ         },
+    {0,         0,          0,      0,              2,              0,              0,              0,                  0,              GET_OPT_ERR_UNKNOWN_ARG_REQ         },
+    {0,         0,          0,      0,              0,              1,              1,              0,                  0,              GET_OPT_ERR_WRONG_BOUNDARIES        },
+    {0,         0,          0,      0,              3,              1,              1,              0,                  0,              GET_OPT_SUCCESS                     },
+    {0,         0,          0,      0,              0,              0,              0,              1,                  0,              GET_OPT_ERR_DEF_VAL_OUT_OF_BOUNDS   },
+    {0,         0,          0,      0,              0,              0,              0,              0,                  1,              GET_OPT_ERR_NULL_DEST_VAR           },
 
 };
 
@@ -161,9 +163,9 @@ int Test_GetOptionDefinition()
                                                         GetOptionDefinition_data.opt_det[           GetOptionDefinition_Test_Indexes[i][2]] ,
                                                         GetOptionDefinition_data.opt_var_type[      GetOptionDefinition_Test_Indexes[i][3]] ,
                                                         GetOptionDefinition_data.opt_needs_arg[     GetOptionDefinition_Test_Indexes[i][4]] ,
-                                                        &GetOptionDefinition_data.opt_min_value[    GetOptionDefinition_Test_Indexes[i][5]] ,
-                                                        &GetOptionDefinition_data.opt_max_value[    GetOptionDefinition_Test_Indexes[i][6]] ,
-                                                        &GetOptionDefinition_data.opt_default_value[GetOptionDefinition_Test_Indexes[i][7]] ,
+                                                        GetOptionDefinition_data.opt_min_value[     GetOptionDefinition_Test_Indexes[i][5]] ,
+                                                        GetOptionDefinition_data.opt_max_value[     GetOptionDefinition_Test_Indexes[i][6]] ,
+                                                        GetOptionDefinition_data.opt_default_value[ GetOptionDefinition_Test_Indexes[i][7]] ,
                                                         GetOptionDefinition_data.opt_dest_var[      GetOptionDefinition_Test_Indexes[i][8]] );
 
         if(get_option_definition != GetOptionDefinition_Test_Indexes[i][9])
@@ -194,7 +196,99 @@ int Test_GetOptionDefinition()
     return test_overall_result;
 }
 
+int option_demo(int argc, char** argv)
+{
+      int c;
+  static int verbose_flag;
+
+  while (1)
+    {
+      static struct option long_options[] =
+        {
+          /* These options set a flag. */
+          {"verbose", no_argument,       &verbose_flag, 1},
+          {"brief",   no_argument,       &verbose_flag, 0},
+          /* These options don’t set a flag.
+             We distinguish them by their indices. */
+          {"add",     no_argument,       0, 'a'},
+          {"append",  no_argument,       0, 'b'},
+          {"delete",  required_argument, 0, 'd'},
+          {"create",  required_argument, 0, 'c'},
+          {"file",    required_argument, 0, 'f'},
+          {0, 0, 0, 0}
+        };
+      /* getopt_long stores the option index here. */
+      int option_index = 0;
+
+      c = getopt_long (argc, argv, "abc:d:f:",
+                       long_options, &option_index);
+
+      /* Detect the end of the options. */
+      if (c == -1)
+        break;
+
+      switch (c)
+        {
+        case 0:
+          /* If this option set a flag, do nothing else now. */
+          if (long_options[option_index].flag != 0)
+            break;
+          printf ("option %s", long_options[option_index].name);
+          if (optarg)
+            printf (" with arg %s", optarg);
+          printf ("\n");
+          break;
+
+        case 'a':
+          puts ("option -a\n");
+          break;
+
+        case 'b':
+          puts ("option -b\n");
+          break;
+
+        case 'c':
+          printf ("option -c with value `%s'\n", optarg);
+          break;
+
+        case 'd':
+          printf ("option -d with value `%s'\n", optarg);
+          break;
+
+        case 'f':
+          printf ("option -f with value `%s'\n", optarg);
+          break;
+
+        case '?':
+          /* getopt_long already printed an error message. */
+          break;
+
+        default:
+          abort ();
+        }
+    }
+
+  /* Instead of reporting ‘--verbose’
+     and ‘--brief’ as they are encountered,
+     we report the final status resulting from them. */
+  if (verbose_flag)
+    puts ("verbose flag is set");
+
+  /* Print any remaining command line arguments (not options). */
+  if (optind < argc)
+    {
+      printf ("non-option ARGV-elements: ");
+      while (optind < argc)
+        printf ("%s ", argv[optind++]);
+      putchar ('\n');
+    }
+
+  exit (0);
+}
+
 int main (int argc, char **argv)
 {
     Test_GetOptionDefinition();
+
+    return 0;
 }
